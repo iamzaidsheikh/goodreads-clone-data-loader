a8d75efc9fd1fb7395bfc13fb2f6c3030efd7817
Zaid <iamzaidsheikh@gmail.com>
2021-11-03 22:39:39 +0530

    build: Add authors data to the db instance

--- src/main/java/com/example/goodreadsclonedataloader/GoodreadsCloneDataLoaderApplication.java
+++ src/main/java/com/example/goodreadsclonedataloader/GoodreadsCloneDataLoaderApplication.java
@@ -1,16 +1,22 @@
 package com.example.goodreadsclonedataloader;
 
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
 
 import javax.annotation.PostConstruct;
 
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.cassandra.CqlSessionBuilderCustomizer;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
-import org.springframework.data.cassandra.repository.config.EnableCassandraRepositories;
 
 import com.example.goodreadsclonedataloader.author.AuthorRepository;
 
@@ -26,6 +32,44 @@ public class GoodreadsCloneDataLoaderApplication {
     @Autowired 
     AuthorRepository authorRepository;
 
+    @Value("${datadump.location.author}")
+    private String authorDumpLocation;
+
+    @Value("${datadump.location.works}")
+    private String worksDumpLocation;
+
+    private void initAuthors() {
+        Path path = Paths.get(authorDumpLocation);
+        try(Stream<String> lines = Files.lines(path)) {
+
+            lines.forEach(line -> {
+                //Read and parse the line
+                String jsonString = line.substring(line.indexOf("{")); 
+                try {
+                    JSONObject jsonObject = new JSONObject(jsonString);
+                    
+                    //Construct Author object 
+                    Author author = new Author();
+                    author.setName(jsonObject.optString("name"));
+                    author.setPersonalName(jsonObject.optString("personal_name"));
+                    author.setId(jsonObject.optString("key").replace("/authors/", ""));
+
+                     //Persist using repository
+                     System.out.println("Saving author " + author.getName() + "...");
+                     authorRepository.save(author);
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+               
+            } );
+
+        }catch(IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
 	public static void main(String[] args) {
 		SpringApplication.run(GoodreadsCloneDataLoaderApplication.class, args);
 	}
@@ -33,11 +77,8 @@ public class GoodreadsCloneDataLoaderApplication {
     //This method runs when the application starts 
     @PostConstruct
     public void start() {
-        Author author = new Author();
-        author.setId("id");
-        author.setName("name");
-        author.setPersonalName("personalName");
-        authorRepository.save(author);
+        initAuthors();
+        
     }
 
 	/**
