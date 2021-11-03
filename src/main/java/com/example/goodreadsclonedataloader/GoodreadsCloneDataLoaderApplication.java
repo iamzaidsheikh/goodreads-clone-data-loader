8a761a77ba861401022e61a6e0ef3e3630e58105
Zaid <iamzaidsheikh@gmail.com>
2021-11-03 16:25:53 +0530

    Initial Setup

---
+++ src/main/java/com/example/goodreadsclonedataloader/GoodreadsCloneDataLoaderApplication.java
@@ -0,0 +1,13 @@
+package com.example.goodreadsclonedataloader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodreadsCloneDataLoaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoodreadsCloneDataLoaderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/goodreadsclonedataloader/GoodreadsCloneDataLoaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.goodreadsclonedataloader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoodreadsCloneDataLoaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
